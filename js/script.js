b17eeaef368bd6c5a04e818352690aa24858bea8
Patrick Zablocki <139109232+PatrickZablocki@users.noreply.github.com>
2025-09-20 17:15:24 +0200

    Neue Sachen ausporbiert

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+// Card Menü für Öffnungzeiten & Anfahrt
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('active');
+  });
+});
+
